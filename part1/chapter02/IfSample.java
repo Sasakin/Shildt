6a93a74ee25a3ccdf69b681a18b8f051a5251928
Sasakin Maxim <max_to_day@Mail.ru>
2016-07-24 22:42:40 +0300

    first commit

---
+++ part1/chapter02/IfSample.java
@@ -0,0 +1,23 @@
+/*
+   Продемонстрировать переменную условного оператора if.
+   Присвоить исходному оператору имя "IfSample.java" 
+*/
+class IfSample{
+	public static void main(String args[]){
+		int x,y;
+
+		x=10;
+		y=20;
+
+		if(x < y) System.out.println("x меньше y");
+
+		x = x * 2;
+		if(x == y) System.out.println("x теперь равно y");
+
+		x = x * 2;
+		if(x > y) System.out.println("x теперь больше y");
+
+		// этот оператор не будет ничего выводить
+		if(x == y) System.out.println("вы не увидите этого");
+	}
+}
