9135a478833127bfd2e828c6186c3e0a2a609dad
Sasakin Maxim <max_to_day@Mail.ru>
2016-09-02 22:59:53 +0300

    collection Task

--- CollectionTask2/src/GrowCollection.java
+++ CollectionTask2/src/GrowCollection.java
@@ -1,23 +1,58 @@
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.ListIterator;
 
 /**
  * Created by Student on 01.09.2016.
  */
-public class GrowCollection<Integer> extends ArrayList<Integer> {
-    @Override
-    public boolean add(int e) {
-        int[] array  = this.toArray(int[] a=new int[this.size()]);
-        GrowCollection<Integer> g = this ;
-        this.clear();
-        for( Integer i:g) {
-            int el = java.lang.Integer(i.toString());
-            super.add(i);
-        }
+public class GrowCollection extends ArrayList {
 
-        //return super.add(e);
-
+    public boolean add(Integer e) {
+        ListIterator<Integer> iter = this.listIterator();
+        while(iter.hasNext()) {
+            Integer element = iter.next()+e;
+            iter.set(element);
         }
+        return super.add(e);
     }
+
+    @Override
+    public boolean add(Object o) {
+        if (this.add(Integer.valueOf(o.toString()))) return true;
+        else return false;
+    }
+
+    public void add(int index, Integer e) {
+        ListIterator<Integer> iter = this.listIterator();
+        while(iter.hasNext()) {
+                Integer element = iter.next() + e;
+                iter.set(element);
+
+        }
+        super.add(index, e);
+    }
+
+    @Override
+    public void add(int index, Object element) {
+        this.add(index, Integer.valueOf(element.toString()));
+    }
+
+    @Override
+    public Object remove(int index) {
+        ListIterator<Integer> iter = this.listIterator();
+        Integer e = Integer.valueOf(this.get(index).toString());
+        while (iter.hasNext()) {
+            Integer element = iter.next()- e;
+            iter.set(element);
+        }
+        return super.remove(index);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+         this.remove(Integer.parseInt(o.toString()));
+         return true;
+    }
+}
 
--- CollectionTask2/src/Main.java
+++ CollectionTask2/src/Main.java
@@ -3,10 +3,16 @@
  */
 public class Main {
     public static void main(String args[]) {
-     GrowCollection<Integer> collection = new GrowCollection<>();
+     GrowCollection collection = new GrowCollection();
         collection.add(1);
-        collection.add(2);
+        collection.add("2");
         collection.add(3);
+        collection.add(0,3);
+        collection.add(1);
+        System.out.println(collection);
+        collection.remove(2);
+        System.out.println(collection);
+        collection.remove("3");
         System.out.println(collection);
     }
 }
